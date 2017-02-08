55413e07babd6b24971ce2017dcf5bb1e3567d8c
Trey Shugart <treshugart@gmail.com>
2017-02-09 09:42:46 +1100

    Update to slides

--- src/eternal-sunshine-of-the-biased-mind/index.js
+++ src/eternal-sunshine-of-the-biased-mind/index.js
@@ -1,10 +1,7 @@
 /* eslint import/no-webpack-loader-syntax: 0 */
 
 import 'skatejs-web-components';
-import { Deck, Slide } from '../_';
+import '@skatejs/vert';
 import html from 'raw-loader!./index.html';
-
-customElements.define('sk-deck', Deck);
-customElements.define('sk-slide', Slide);
 
 document.getElementById('root').innerHTML = html;
