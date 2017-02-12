1f07c5a6e86c61fd71845aa05271edd5d46d50c2
Trey Shugart <treshugart@gmail.com>
2017-02-12 16:07:19 +1100

    Progress on ESOTSM.

--- src/eternal-sunshine-of-the-biased-mind/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint import/no-webpack-loader-syntax: 0 */
-
-import 'skatejs-web-components';
-import '@skatejs/vert';
-import html from 'raw-loader!./index.html';
-
-document.getElementById('root').innerHTML = html;
